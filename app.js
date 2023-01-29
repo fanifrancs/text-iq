e7db4e40ae367d75f3def8b96c27bbbb58092c6b
fanifrancs <fanikufran6@gmail.com>
2023-01-29 03:41:55 +0100

    clean up

--- app.js
+++ app.js
@@ -1,51 +1,60 @@
 const inputfield = document.querySelector('#input'),
 placeholder = "Paste / enter input here";
-let input, find, repl, regex;
+let input, find, repl, regex, modifier;
+
+inputfield.addEventListener('input', () => {
+    nkeystr();
+    nchar();
+    nword();
+    ucase();
+    lcase();
+})
 
 function nkeystr() {
+    // sets input variable in global scope to input field's value
     input = inputfield.value;
     // stores the input in the localStorage as user paste/input it
     // so the input can be accessed in case of accidental closing of
     // browser tab or reloading
     localStorage.setItem('input', input);
+    if (!input) return document.querySelector('#nkeystr').innerText = '0';
     let output = input.length;
     document.querySelector('#nkeystr').innerText = output; 
 }
 
-function nword() {
-    let output = input.match(/(\w+)/g).length;
-    document.querySelector('#nword').innerText = output;
-}
-
 function nchar() {
+    if (!input) return document.querySelector('#nchar').innerText = '0';
     let output = input.replace(/\s+/g, '').length;
     document.querySelector('#nchar').innerText = output; 
 }
 
+function nword() {
+    if (!input) return document.querySelector('#nword').innerText = '0';
+    let output = input.match(/(\w+)/g).length;
+    document.querySelector('#nword').innerText = output;
+}
+
 function ucase() {
-    output = input.toUpperCase();
+    let output = input.toUpperCase();
     document.querySelector('#ucase').innerText = output;
 }
 
 function lcase() {
-    output = input.toLowerCase();
+    let output = input.toLowerCase();
     document.querySelector('#lcase').innerText = output;
 }
 
-function fnri() {
-    find = document.querySelector('#findi').value;
-    repl = document.querySelector('#repli').value;
-    regex = new RegExp(find, 'gi');
+function fnr() {
+    find = document.querySelector('#find').value;
+    repl = document.querySelector('#repl').value;
+    modifier = document.querySelector('#modifier').value
+    if (modifier === 'case-sensitive') {
+        regex = new RegExp(find, 'g');
+    } else {
+        regex = new RegExp(find, 'gi');
+    }
     let output = input.replace(regex, repl);
-    document.querySelector('#fnri').innerText = output;
-}
-
-function fnrs() {
-    find = document.querySelector('#finds').value;
-    repl = document.querySelector('#repls').value;
-    regex = new RegExp(find, 'g')
-    let output = input.replace(regex, repl);
-    document.querySelector('#fnrs').innerText = output;
+    document.querySelector('#fnr').innerText = output;
 }
 
 function reset() {
