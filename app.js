ce9e537128574acb1edc947e5159dab3a471aa7f
fanifrancs <fanikufran6@gmail.com>
2023-01-30 01:20:13 +0100

    update

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ const inputfield = document.querySelector('#input'),
 placeholder = "Paste / enter input here";
 let input, find, repl, regex, modifier;
 
-inputfield.addEventListener('input', () => {
-    nkeystr();
-    nchar();
-    nword();
-    ucase();
-    lcase();
-})
+inputfield.addEventListener('input', nkeystr)
 
 function nkeystr() {
     // sets input variable in global scope to input field's value
     input = inputfield.value;
+    nchar();
     // stores the input in the localStorage as user paste/input it
     // so the input can be accessed in case of accidental closing of
     // browser tab or reloading
@@ -23,18 +18,21 @@ function nkeystr() {
 }
 
 function nchar() {
+    nword();
     if (!input) return document.querySelector('#nchar').innerText = '0';
     let output = input.replace(/\s+/g, '').length;
-    document.querySelector('#nchar').innerText = output; 
+    document.querySelector('#nchar').innerText = output;
 }
 
 function nword() {
+    ucase();
     if (!input) return document.querySelector('#nword').innerText = '0';
     let output = input.match(/(\w+)/g).length;
     document.querySelector('#nword').innerText = output;
 }
 
 function ucase() {
+    lcase();
     let output = input.toUpperCase();
     document.querySelector('#ucase').innerText = output;
 }
@@ -75,8 +73,4 @@ if (storedinput === null) {
 } else {
     inputfield.value = storedinput;
     nkeystr();
-    nchar();
-    nword();
-    ucase();
-    lcase();
 }
